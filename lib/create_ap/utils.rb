09297b2510d643c9a3eb5f26df6d65647b723036
oblique <psyberbits@gmail.com>
2016-11-03 00:41:54 +0200

    Move CreateAp::Utils::* to CreateAp

--- lib/create_ap/utils.rb
+++ lib/create_ap/utils.rb
@@ -1,25 +1,23 @@
 require 'create_ap/subprocess'
 
 module CreateAp
-  module Utils
-    def self.run(*args)
-      p = Subprocess.new(*args)
-      status = p.each do |line|
-        if block_given?
-          yield line
-        else
-          Log.info "#{p.exe}[#{p.pid}]: #{line}"
-        end
+  def self.run(*args)
+    p = Subprocess.new(*args)
+    status = p.each do |line|
+      if block_given?
+        yield line
+      else
+        Log.info "#{p.exe}[#{p.pid}]: #{line}"
       end
-      status&.exitstatus == 0
     end
+    status&.success?
+  end
 
-    def self.which(cmd)
-      ENV['PATH'].split(File::PATH_SEPARATOR).each do |path|
-        exe = File.join(path, cmd)
-        return exe if File.executable?(exe) && !File.directory?(exe)
-      end
-      nil
+  def self.which(cmd)
+    ENV['PATH'].split(File::PATH_SEPARATOR).each do |path|
+      exe = File.join(path, cmd)
+      return exe if File.executable?(exe) && !File.directory?(exe)
     end
+    nil
   end
 end
