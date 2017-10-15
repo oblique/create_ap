0523b4aab3a0175c88d7ff82bb9189a30c8144a1
bauke molenaar <bauke.molenaar@gmail.com>
2017-10-15 12:01:47 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -20,13 +20,13 @@ echo "edit /etc/create_ap.conf"
 echo "make sure to set the right values"
 sudo nano /etc/create_ap.conf
 
-echo "making create_ap start at boot"
-systemctl enable create_ap
-echo "create_ap starts at boot"
+#uncomment this tomake create_ap start at boot"
+#echo "making create_ap start at boot"
+#systemctl enable create_ap
 
 #uncomment this to start create_ap with systemctl 
 #echo "start create_ap now (with settings from /etc/create_ap.conf)"
 #systemctl start create_ap
 
-#uncomment this to manually start create_ap
+#uncomment this to manually start create_ap with the $BBSID and $WPA2PASS provided at the start of this script
 #create_ap wlan0 eth0 $BSSID $WPA2PASS
