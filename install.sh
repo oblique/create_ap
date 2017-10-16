49e94c152b92bf02fe6de62fe594a967d4bb1890
bauke molenaar <bauke.molenaar@gmail.com>
2017-10-16 19:24:34 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 BSSID="h0tsp0t"
 WPA2PASS="supersecretpassword"
-DEPS="git bash util-linux dnsmasq iptables procps hostapd iproute iw iwconig haveged"
+DEPS="git bash util-linux dnsmasq isc-dhcp-server net-tools iptables procps hostapd iproute iw haveged"
 
 ####################################################
 ### create_ap installation script made by rizzo  ###
@@ -22,11 +22,11 @@ sudo nano /etc/create_ap.conf
 
 #uncomment this to make create_ap start at boot"
 #echo "making create_ap start at boot"
-#systemctl enable create_ap
+systemctl enable create_ap
 
 #uncomment this to start create_ap with systemctl 
 #echo "start create_ap now (with settings from /etc/create_ap.conf)"
-#systemctl start create_ap
+systemctl start create_ap
 
 #uncomment this to manually start create_ap with the $BBSID and $WPA2PASS provided at the start of this script
-#create_ap wlan0 eth0 $BSSID $WPA2PASS
+create_ap wlan0 eth0 $BSSID $WPA2PASS
