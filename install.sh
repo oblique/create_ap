53c33878c0d3a54ae13e34c335417c3d53b08408
hans molenaar <hans@DESKTOP-E98KFQ4--hans.fritz.box>
2017-10-23 19:56:27 +0200

    modified:   install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 BSSID="h0tsp0t"
 WPA2PASS="supersecretpassword"
-DEPS="git bash util-linux dnsmasq isc-dhcp-server net-tools iptables procps hostapd iproute iw haveged"
+DEPS="git bash util-linux procps hostapd iproute iw haveged  dnsmasq isc-dhcp-server net-tools iptables"
 
 ####################################################
 ### create_ap installation script made by rizzo  ###
@@ -10,6 +10,7 @@ DEPS="git bash util-linux dnsmasq isc-dhcp-server net-tools iptables procps host
 echo "installing dependencies"
 sudo apt-get install $DEPS
 
+cd ~
 echo "cloning create_ap git"
 git clone https://github.com/itsdarklikehell/create_ap
 cd create_ap
@@ -24,9 +25,11 @@ sudo nano /etc/create_ap.conf
 #echo "making create_ap start at boot"
 systemctl enable create_ap
 
-#uncomment this to start create_ap with systemctl 
+#uncomment this to start create_ap with systemctl
 #echo "start create_ap now (with settings from /etc/create_ap.conf)"
 systemctl start create_ap
 
 #uncomment this to manually start create_ap with the $BBSID and $WPA2PASS provided at the start of this script
-sudo create_ap wlan0 eth0 $BSSID $WPA2PASS
+#sudo create_ap wlan0 eth0 $BSSID $WPA2PASS
+
+echo "if all succedded there should now be a network with the supplied config"
