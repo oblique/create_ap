6656712649f4c248c0284fa8f5a66bf409e56c63
caozuohua <535863761@qq.com>
2018-01-22 17:28:02 +0800

    test build in a newly installed os

--- debian/createap.cron.d.ex
+++
@@ -1,4 +0,0 @@
-#
-# Regular cron jobs for the createap package
-#
-0 4	* * *	root	[ -x /usr/bin/createap_maintenance ] && /usr/bin/createap_maintenance
--- debian/menu.ex
+++
@@ -1,2 +0,0 @@
-?package(createap):needs="X11|text|vc|wm" section="Applications/see-menu-manual"\
-  title="createap" command="/usr/bin/createap"
