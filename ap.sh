6691aeb5230e5f9457f3172e44b75c744b0369ab
Diveyez <nemofbaltimore@gmail.com>
2018-03-18 09:53:52 -0400

    Update ap.sh

--- ap.sh
+++ ap.sh
@@ -1,13 +1,3 @@
 #!/bin/bash
 # Reads for User Input
-echo "Because of vulnerabilities with other security, we will use WPA2"
-echo "What Name Do You Want The WiFi To Have?:"
-read ssid
-echo "What Password Should It Have?:"
-read key
-echo "What Channel?: (To Avoid Congestion, Use A Channel Not Popular In The Area, ie: 3,5,7,9)"
-read channel
-echo "What Interface Do We Use?:"
-read wifidev
-# Launch Time
-sudo create_ap -w 2 -c $channel $wifdev eth0 $ssid $key
+echo "Did not work, so I deleted the code"
