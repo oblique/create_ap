c59b31be71bad485b6bb1d0f21d143ea3f44e35f
Diveyez <nemofbaltimore@gmail.com>
2018-03-18 06:20:49 -0400

    Update ap.sh

--- ap.sh
+++ ap.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 # Reads for User Input
+echo "Because of vulnerabilities with other security, we will use WPA2"
 echo "What Name Do You Want The WiFi To Have?:"
 read ssid
-echo "What PSK/Password Should It Have?:"
+echo "What Password Should It Have?:"
 read key
 echo "What Channel?: (To Avoid Congestion, Use A Channel Not Popular In The Area, ie: 3,5,7,9)"
 read channel
