815181db42d22cb5510236093f6417005af2e6fa
Diveyez <nemofbaltimore@gmail.com>
2018-04-03 02:36:40 -0400

    ILLUMINATI

--- ap.sh
+++ ap.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
-# Reads for User Input
-echo "Did not work, so I deleted the code"
+nmcli r wifi off
+rfkill unblock wlan
+killall -9 wpa_supplicant
+screen -d -m -S  ap create_ap -w 2 -c 9 wlan0 eth0 Illuminati confirmed1337!
+# 
+echo "You now have Illuminati AP setup on channel 9 with the password confirmed1337!"
