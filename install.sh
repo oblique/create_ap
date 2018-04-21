7c94d362cb362bc244cf0adbf497dee6b87a247c
bauke molenaar <bauke.molenaar@gmail.com>
2018-04-21 18:40:58 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -28,19 +28,22 @@ read WPA2PASS
 
 #uncomment this to manually start create_ap with the $BBSID and $WPA2PASS provided at the start of this script
 sudo create_ap $APFC $INTER $BSSID $WPA2PASS
+echo ""
 echo "there should now be a network called $BSSID on $APFC connected with $INTER now."
 read -rsp $'Press enter to continue...\n'
 
-echo "edit /etc/create_ap.conf"
-echo "make sure to set the right values"
+echo "edit /etc/create_ap.conf to make a static config"
+echo "please make sure to set the right values."
+read -rsp $'Press enter to continue...\n'
 sudo nano /etc/create_ap.conf
 
 #uncomment this to make create_ap start at boot"
 echo "making create_ap start at boot"
+read -rsp $'Press enter to continue...\n'
 sudo systemctl enable create_ap
 
 #uncomment this to start create_ap with systemctl
-#echo "start create_ap now (with settings from /etc/create_ap.conf)"
+echo "starting create_ap now (with settings from /etc/create_ap.conf)"
+read -rsp $'Press enter to continue...\n'
 sudo systemctl start create_ap
-
-echo "if all succedded there should now be a network with the supplied config"
+echo "if all succedded there should now be a network with the supplied config in /etc/create_ap.conf"
