44774fc0fafe8bcfb662dc27043152d24c5d7d60
Diveyez <nemofbaltimore@gmail.com>
2018-06-07 00:26:53 -0700

    ILLUMINATI CONFIRMED

--- ap.sh
+++ ap.sh
@@ -4,4 +4,4 @@ rfkill unblock wlan
 killall -9 wpa_supplicant
 screen -d -m -S  ap create_ap -w 2 -c 9 wlan0 eth0 Illuminati confirmed1337!
 # 
-echo "You now have Illuminati AP setup on channel 9 with the password confirmed1337!"
+echo "You now have 'Illuminati' AP setup on channel 9 with the password 'confirmed1337!' "
