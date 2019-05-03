fb91f2ba2293b8a0ec39a2251cd605306beff093
Diveyez <nemofbaltimore@gmail.com>
2019-05-03 08:13:19 -0700

    using network manager cli to do the initial steps

--- install-dep.sh
+++ install-dep.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-sudo apt install util-linux procps-ng hostapd iproute2 iw iwconfig haveged dnsmasq iptables -y
+sudo apt install util-linux procps-ng hostapd iproute2 iw iwconfig haveged dnsmasq iptables network-manager -y
 apt-get install build-essential dkms git linux-headers -y #linux-headers-generic
 apt-get remove hostapd -y
 apt-get build-dep hostapd -y
