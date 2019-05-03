4436a587fab0a16484c78054cf64e1e4e8893f13
Diveyez <nemofbaltimore@gmail.com>
2019-05-03 08:08:42 -0700

    .

--- install-dep.sh
+++ install-dep.sh
@@ -1,2 +1,5 @@
 #!/bin/bash
-sudo apt install util-linux procps-ng hostapd iproute2 iw iwconfig haveged dnsmasq iptables
+sudo apt install util-linux procps-ng hostapd iproute2 iw iwconfig haveged dnsmasq iptables -y
+apt-get install build-essential dkms git linux-headers -y #linux-headers-generic
+apt-get remove hostapd -y
+apt-get build-dep hostapd -y
